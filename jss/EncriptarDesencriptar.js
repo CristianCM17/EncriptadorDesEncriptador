384d405e70a8f03c8c49e93bef134e8afc425f0f
CristianCM17 <cristiandavidcardoso@gmail.com>
2023-05-28 14:14:54 -0600

    algoritmo terminado

--- jss/EncriptarDesencriptar.js
+++ jss/EncriptarDesencriptar.js
@@ -6,7 +6,10 @@
 const btnEncriptar= document.getElementById("btn-encriptar");
 const frase= document.getElementById("frase");
 const respuesta = document.getElementById("respuesta");
+respuesta.readOnly=true;
 const btnlimpiar = document.getElementById("btn-limpiar");
+const btnDesencriptar = document.getElementById("btn-desencriptar")
+const btnCopiar = document.getElementById("btn-copiar");
 
 
 
@@ -42,15 +45,46 @@ function encriptar() {
       return nuevaPalabra;
     }
 
-   function addResp() {
+    function Desencriptar() {
+      
+        let fraseDesencriptada = frase.value.replace(/ai/g, 'a')
+            .replace(/enter/g, 'e')
+            .replace(/imes/g, 'i')
+            .replace(/ober/g, 'o')
+            .replace(/ufat/g, 'u');
+        
+            return fraseDesencriptada;
+            
+
+
+    }
+
+   function addEncr() {
         respuesta.innerHTML= encriptar()
+        
+   }
+   function addDesc(){
+    respuesta.innerHTML= Desencriptar()
    }
 
-   btnEncriptar.onclick= addResp;
+   btnEncriptar.onclick= addEncr;
+   btnDesencriptar.onclick=addDesc
 
    btnlimpiar.addEventListener("click", () => {
             frase.value= '';
+           
    });
+
+   btnCopiar.addEventListener("click", function() {
+    respuesta.select();
+    navigator.clipboard.writeText(respuesta.value)
+      .then(function() {
+        alert("El contenido se ha copiado al portapapeles.");
+      })
+      .catch(function(error) {
+        console.error("Error al copiar al portapapeles: ", error);
+      });
+  });
 
    
 
