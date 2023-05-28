f637797049695957e9744aa7050e662c9e49138e
CristianCM17 <cristiandavidcardoso@gmail.com>
2023-05-28 14:41:09 -0600

    validaciones

--- jss/EncriptarDesencriptar.js
+++ jss/EncriptarDesencriptar.js
@@ -3,23 +3,23 @@
 
 
 
-const btnEncriptar= document.getElementById("btn-encriptar");
-const frase= document.getElementById("frase");
-const respuesta = document.getElementById("respuesta");
-respuesta.readOnly=true;
-const btnlimpiar = document.getElementById("btn-limpiar");
-const btnDesencriptar = document.getElementById("btn-desencriptar")
-const btnCopiar = document.getElementById("btn-copiar");
+const btnEncriptar= document.getElementById("btn-encriptar");//accedemps al boton que encriptara
+const frase= document.getElementById("frase"); // accedemos a la frase que encriptaremos o desencriptaremos del text area
+const respuesta = document.getElementById("respuesta");// accedemos a la respuesta que sera la desencriptacion o encriptacion
+respuesta.readOnly=true; //el text area de la respuesta no se podra excribir nada
+const btnlimpiar = document.getElementById("btn-limpiar"); //accedemos a la btnlimpiar que limpiara la frase
+const btnDesencriptar = document.getElementById("btn-desencriptar")//accedemos al boton que desencriptara
+const btnCopiar = document.getElementById("btn-copiar");//accedemos a la btnCopiar que copiara el contenido de la respuesta
 
 
 
 
 
-function encriptar() {
-    let nuevaPalabra= '';
-
+function encriptar() {  //algoritmo que encripata
+    let nuevaPalabra= ''; //la nueva palabra ira completandose en el loop
+        if(frase.value!==''){
     for (let index = 0; index < frase.value.length; index++) {
-        switch (frase.value[index]) {
+        switch (frase.value[index]) { //en caso de que tenga la frase alguna delas letras pasara eso
             case 'a':
                 nuevaPalabra=nuevaPalabra+'ai'
                 break;
@@ -37,52 +37,67 @@ function encriptar() {
                                 break;
         
             default:
-                nuevaPalabra=nuevaPalabra+frase.value[index];
+                nuevaPalabra=nuevaPalabra+frase.value[index]; //si no la regresara completa
                 break;
         }
         
       }
-      return nuevaPalabra;
+      return nuevaPalabra; // aqui debe ya estar encriptado que es lo que retornaremos en caso de que se cumplan las condiciones
+      }
+      else {
+        alert("no hay frase")
+      }
     }
 
-    function Desencriptar() {
+    function Desencriptar() { //algorimo que me desencripta
       
-        let fraseDesencriptada = frase.value.replace(/ai/g, 'a')
-            .replace(/enter/g, 'e')
-            .replace(/imes/g, 'i')
-            .replace(/ober/g, 'o')
-            .replace(/ufat/g, 'u');
-        
-            return fraseDesencriptada;
+        //la frase desencriptada sera igual a mi frase pero le replazaremos a lo que equivale cada letra
+        if (frase.value!==''){  let fraseDesencriptada = frase.value.replace(/ai/g, 'a')
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    
+        return fraseDesencriptada; //retornamos la frase desencriptada}
+      
             
 
 
+    }else {
+        alert("No hay frase que desencriptar")
+     }
     }
-
    function addEncr() {
-        respuesta.innerHTML= encriptar()
-        
+    if(typeof encriptar() === 'undefined'){
+        respuesta.innerHTML= '' //aqui agregamos lo que retorna la funcion encriptar    
+   }else{
+    respuesta.innerHTML= encriptar();
    }
+}
    function addDesc(){
-    respuesta.innerHTML= Desencriptar()
+    if(typeof Desencriptar() === 'undefined'){
+     respuesta.innerHTML= '';
+   }else{
+    respuesta.innerHTML= Desencriptar()//aqui agregamos lo que retorna la fucnion desencriptar
    }
+ }
 
-   btnEncriptar.onclick= addEncr;
-   btnDesencriptar.onclick=addDesc
+   btnEncriptar.onclick= addEncr;// elclick llamara a la funcion addEcnr que lo gregara al campo de la respuesta
+   btnDesencriptar.onclick=addDesc // el click llaara a la funcion addDesc que lo gregara al campo de la respuesta
 
    btnlimpiar.addEventListener("click", () => {
-            frase.value= '';
+            frase.value= ''; //la frase se reemplazara por vacio
            
    });
 
    btnCopiar.addEventListener("click", function() {
-    respuesta.select();
-    navigator.clipboard.writeText(respuesta.value)
+    respuesta.select(); //seleccionara lo contenido en la respuesta
+    navigator.clipboard.writeText(respuesta.value) //lo copiara al portapapeles
       .then(function() {
-        alert("El contenido se ha copiado al portapapeles.");
+        alert("El contenido se ha copiado al portapapeles."); //si todo marcha bien 
       })
       .catch(function(error) {
-        console.error("Error al copiar al portapapeles: ", error);
+        console.error("Error al copiar al portapapeles: ", error); //si hay error
       });
   });
 
