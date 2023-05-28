1dc9a3d287000399a9fb8ea5e11ed8f6f2d8bdc8
CristianCM17 <cristiandavidcardoso@gmail.com>
2023-05-28 13:20:05 -0600

    Desencriptacion

---
+++ Encriptar.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+const btnEncriptar= document.getElementById("btn-encriptar");
+const frase= document.getElementById("frase");
+const respuesta = document.getElementById("respuesta");
+
+function encriptar() {
+    let nuevaPalabra= '';
+
+    for (let index = 0; index < frase.value.length; index++) {
+        switch (frase.value[index]) {
+            case 'a':
+                nuevaPalabra=nuevaPalabra+'ai'
+                break;
+                case 'e':
+                    nuevaPalabra=nuevaPalabra+'enter'
+                    break;
+                    case 'o':
+                        nuevaPalabra=nuevaPalabra+'ober'
+                        break;
+                        case 'i':
+                            nuevaPalabra=nuevaPalabra+'imes'
+                            break;
+                            case 'u':
+                                nuevaPalabra=nuevaPalabra+'ufat'
+                                break;
+        
+            default:
+                nuevaPalabra=nuevaPalabra+frase.value[index];
+                break;
+        }
+        
+      }
+      return nuevaPalabra;
+    }
+
+   function addResp() {
+        respuesta.innerHTML= encriptar()
+   }
+
+   btnEncriptar.onclick= addResp;
+
+   
+
+
+
+
+
+
